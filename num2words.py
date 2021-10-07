a49786413dccdac84b2e61e0e39a11d95c2fe748
c0ntract0r <83899705+c0ntract0r@users.noreply.github.com>
2021-10-07 05:26:56 -0700

    Updated to work only with AMD(Armenian Dram)

--- num2words.py
+++ num2words.py
@@ -1,13 +1,13 @@
-under_20 = ['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Eleven','Twelve','Thirteen','Fourteen','Fifteen','Sixteen','Seventeen','Eighteen','Nineteen'] 
-tens = ['', '','Twenty','Thirty','Forty','Fifty','Sixty','Seventy','Eighty','Ninety'] 
-above_100 = {100: 'Hundred',1000:'Thousand', 1000000:'Million', 1000000000:'Billion'} 
+under_20 = ['զրո','մեկ','երկու','երեք','չորս','հինգ','վեց','յոթ','ութ','ինը','տաս'] 
+tens = ['', 'տասն','քսան','երեսուն','քառասուն','հիսուն','վաթսուն','յոթանասուն','ութանասուն','իննսուն'] 
+above_100 = {100: 'հարյուր',1000:'հազար', 1000000:'միլլիոն', 1000000000:'միլլիարդ'} 
 
 #main logic start
 def NumToWord(num): 
-    if num < 20: 
+    if num <= 10: 
         return under_20[num]  
     elif num < 100: 
-    	return tens[(int)(num/10)] + ('' if num%10==0 else ' ' + under_20[num%10])
+    	return tens[(int)(num/10)] + ('' if num%10==0 else '' + under_20[num%10])
 
     pivot = max([key for key in above_100.keys() if key <= num]) 
     return NumToWord((int)(num/pivot)) + ' ' + above_100[pivot] + ('' if num%pivot==0 else ' ' + NumToWord(num%pivot))
@@ -16,5 +16,5 @@ def NumToWord(num):
 val = float(input("Enter money value: "))
 dec_point = int(val)
 float_point = int(round(val - dec_point, 2) * 100)
-print(NumToWord(dec_point), 'dollars,', NumToWord(float_point), 'cents.\n')
+print(NumToWord(dec_point), 'դրամ,', NumToWord(float_point), 'լումա.\n')
 
